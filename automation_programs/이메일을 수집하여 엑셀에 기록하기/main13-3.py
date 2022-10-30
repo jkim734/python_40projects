70acc6947e889390ce9fe81f17a39c6e5006f342
김재현 <71424602+jkim734@users.noreply.github.com>
2022-10-30 06:57:22 +0000

    -

--- automation_programs/이메일을 수집하여 엑셀에 기록하기/main13-3.py
+++ automation_programs/이메일을 수집하여 엑셀에 기록하기/main13-3.py
@@ -7,7 +7,7 @@ headers = {
     "User-Agent": "Mozilla/5.0",
     "Content-Type": "text/html; charset = utf-8"
     }
-    
+
 response = requests.get(url, headers = headers)
 results = re.findall(r'[\w\.-]+@[\w\.-]+', response.text)
 results= list(set(results))
